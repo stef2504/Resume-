15f34f951893ff17fc1934ece23b1f7e4cac540c
stef regeling <stefregeling@imac-2.home>
2022-02-13 15:01:10 +0100

    added javscript sidebar

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+/* set the width on the side navigation bar to 250px */
+function openNav(){
+    document.getElementById("mysidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closenav(){
+    document.getElementById("mysidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft ="0";
+    document.body.style.backgroundColor = "white";
+}
