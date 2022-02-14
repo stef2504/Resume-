689594de7ae717c7fc75dfbd7478fa525b4b2ccc
stef regeling <stefregeling@imac-2.home>
2022-02-14 20:43:21 +0100

    added profile page

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,7 @@
 function openNav(){
     document.getElementById("mysidenav").style.width = "300px";
     document.getElementById("main").style.marginLeft = "300px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
 }
 
 function closenav(){
