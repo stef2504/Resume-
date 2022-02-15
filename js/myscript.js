2de1337f1ea0c638fcef8ed33c7843d7ce74ab84
stef2504 <stefregeling@gmail.com>
2022-02-15 21:16:57 +0100

    created navbar

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
-/* set the width on the side navigation bar to 250px */
+/* set the width on the side navigation bar to 250px 
 function openNav(){
     document.getElementById("mysidenav").style.width = "300px";
     document.getElementById("main").style.marginLeft = "300px";
@@ -9,4 +9,17 @@ function closenav(){
     document.getElementById("mysidenav").style.width = "0";
     document.getElementById("main").style.marginLeft ="0";
     document.body.style.backgroundColor = "white";
+} */
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction (){
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
+{
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("picture").style.fontsize = "25px";
+} else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("picture").style.fontsize = "35px";
+}
 }
