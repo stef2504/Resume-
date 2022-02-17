aca0d88b248778440613b0b8c8f0fe8174638cf6
stef2504 <stefregeling@gmail.com>
2022-02-17 21:44:36 +0100

    new profile tab

--- js/myscript.js
+++ js/myscript.js
@@ -16,10 +16,8 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction (){
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
 {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("picture").style.fontsize = "25px";
+    document.getElementById("navbar").style.padding = "20px 10px";
 } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("picture").style.fontsize = "35px";
+    document.getElementById("navbar").style.padding = "50px 10px";
 }
 }
